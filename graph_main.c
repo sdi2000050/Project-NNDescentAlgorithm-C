c4d1557cb177fc1ba8c6a37c6c9c1930e9ae2870
John Doe <sdi2000050@di.uoa.gr>
2023-11-03 20:08:02 +0200

    final

--- graph_main.c
+++ graph_main.c
@@ -7,18 +7,20 @@
 int main(int argc, char *argv[]) {
 
     char *input_file = NULL;
+    char *input_point;
     int numnodes;
     int dim = 0;
     int k = 0;
 
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s input_file dimension k\n", argv[0]);
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s input_file file_point dimension k\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     input_file = argv[1];
-    dim = atoi(argv[2]);
-    k = atoi(argv[3]); 
+    input_point = argv[2];
+    dim = atoi(argv[3]);
+    k = atoi(argv[4]); 
 
     if (dim <= 0 || k<=0) {
         fprintf(stderr, "Invalid number of nodes or dimension.\n");
@@ -36,18 +38,73 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
     Distancefunc dis;
-    printNeighbors(graph);
     printf("Press 1 for euclidean distance\nPress 2 for manhattan distance\nPress 3 for chebyshev distance \n");
     int dis_num;
     scanf("%d", &dis_num);
-    if(dis_num == 1) {
-        nndescent(graph,k,euclidean_distance);    
+    int nn_num;
+    printf("Press 1 for nn descent algorithm for the whole graph\nPress 2 for nn descent algorithm for one point\n");
+    scanf("%d", &nn_num);
+    if(nn_num == 1){
+        if(dis_num == 1) {
+            nndescent(graph,k,euclidean_distance);    
+        }
+        else if(dis_num == 2) {
+            nndescent(graph,k,manhattan_distance);    
+        }
+        else if(dis_num == 3) {
+            nndescent(graph,k,chebyshev_distance);    
+        }
+        else{
+            printf("Invalid input\n");
+        }
     }
-    if(dis_num == 2) {
-        nndescent(graph,k,manhattan_distance);    
-    }
-    if(dis_num == 3) {
-        nndescent(graph,k,chebyshev_distance);    
+    else if(nn_num==2){
+        point* p = malloc(sizeof(point));
+        FILE* file = fopen(input_point,"rb");
+        if (file == NULL) {
+            fprintf(stderr, "Error opening file: %s\n", input_point);
+            exit(EXIT_FAILURE);
+        }
+        
+        if (p == NULL) {
+            fprintf(stderr, "Memory allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+
+        float* values = malloc(dim * sizeof(float));
+        if (values == NULL) {
+            fprintf(stderr, "Memory allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+
+        
+        if (fread(values, sizeof(float), dim, file) != dim) {         // Read the floats coords for the current point from the file
+            fprintf(stderr, "Error reading data from file\n");
+            exit(EXIT_FAILURE);
+        }
+
+        setcoords(p,values,dim);
+        printf("Give starting node out of %d\n",numnodes);
+        int n;
+        scanf("%d",&n);
+        KDistance** kd;
+        if(dis_num == 1) {
+            kd = nndescentpoint(graph,k,n,*p,euclidean_distance);    
+        }
+        else if(dis_num == 2) {
+            kd =nndescentpoint(graph,k,n,*p,manhattan_distance);    
+        }
+        else if(dis_num == 3) {
+            kd = nndescentpoint(graph,k,n,*p,chebyshev_distance);    
+        }
+        else{
+            printf("Invalid input\n");
+        }
+        printf("%d nearest nodes to point:\n",k);
+        for(int i; i<k; i++){
+            printf("%d ",kd[i]->node->numnode);
+        }
+        printf("\n");
     }
 
 
